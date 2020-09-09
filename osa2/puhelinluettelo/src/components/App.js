77d1d75f32ab17c2031a3332e81d82b0657816a1
Ville Saarinen <ville.saarinen9@gmail.com>
2020-09-09 13:32:45 +0300

    osa2 2.6 puhelinluettelo, init

---
+++ osa2/puhelinluettelo/src/components/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+const Header = ({text}) => <h2>{text}</h2>
+
+const Numbers = ({numbers}) => {
+    return (
+        <div>
+            {numbers.map(number => <Number key={number.name} number={number} />)}
+        </div>
+    )
+}
+
+const Number = ({number}) => {
+    return (
+        <p>{number.name}</p>
+    )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+      event.preventDefault()
+      setPersons(persons.concat({name: newName}))
+      setNewName('')
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+
+  return (
+    <div>
+        <Header text="Phonebook" />
+
+        <form onSubmit={addPerson}>
+            <div>
+                name: <input value={newName} onChange={handleNameChange} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+
+        <Header text="Numbers" />
+        
+        <Numbers numbers={persons} />
+
+    </div>
+  )
+
+}
+
+export default App
