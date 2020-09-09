03818dca8e842443753e16674c2b577d2bdc0d60
Ville Saarinen <ville.saarinen9@gmail.com>
2020-09-09 14:08:22 +0300

    osa2 2.9 puhelinluettelo, name filtering

--- osa2/puhelinluettelo/src/components/App.js
+++ osa2/puhelinluettelo/src/components/App.js
@@ -17,52 +17,65 @@ const Number = ({number}) => {
 }
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-    number:'044-3111725' }
-  ])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
+    const [ persons, setPersons ] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ])
 
-  const addPerson = (event) => {
-      event.preventDefault()
+    const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
 
-      if (persons.find(elem => elem.name === newName) !== undefined) {
+    const [ nameFilter, setNameFilter ] = useState('')
+
+    const addPerson = (event) => {
+        event.preventDefault()
+
+        if (persons.find(elem => elem.name === newName) !== undefined) {
         window.alert(`${newName} is already added to the phonebook!`)
-      }
-      else {
+        }
+        else {
         setPersons(persons.concat({name: newName, number: newNumber}))
         setNewName('')
         setNewNumber('')
-      }
-  }
+        }
+    }
 
-  const handleNameChange = (event) => setNewName(event.target.value)
-  const handleNumberChange = (event) => setNewNumber(event.target.value)
+    const handleNameChange = (event) => setNewName(event.target.value)
+    const handleNumberChange = (event) => setNewNumber(event.target.value)
 
-  return (
-    <div>
-        <Header text="Phonebook" />
+    const handleFilterChange = (event) => {
+        setNameFilter(event.target.value)
+    }
 
-        <form onSubmit={addPerson}>
-            <div>
-                Name: <input value={newName} onChange={handleNameChange} />
-            </div>
-            <div>
-                Number: <input value={newNumber} onChange={handleNumberChange} />
-            </div>
-            <div>
-                <button type="submit">add</button>
-            </div>
-        </form>
+    return (
+        <div>
+            <Header text="Phonebook" />
 
-        <Header text="Numbers" />
-        
-        <Numbers numbers={persons} />
+            Filter names with <input value={nameFilter} onChange={handleFilterChange} />
 
-    </div>
-  )
+            <Header text="Add new number" />
 
-}
+            <form onSubmit={addPerson}>
+                <div>
+                    Name: <input value={newName} onChange={handleNameChange} />
+                </div>
+                <div>
+                    Number: <input value={newNumber} onChange={handleNumberChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+
+            <Header text="Numbers" />
+            
+            <Numbers numbers={persons.filter(num => num.name.toLowerCase().includes(nameFilter))} />
+
+        </div>
+    )
+
+    }
 
 export default App
