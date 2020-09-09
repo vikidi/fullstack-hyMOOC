ffab64f68381f8657a8c2f5ee2545e3b3b53a1bf
Ville Saarinen <ville.saarinen9@gmail.com>
2020-09-09 11:18:55 +0300

    osa2 2.5 kurssitiedot, separate module

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,55 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-/* FUNCTIONS */
-
-
-
-/* COMPONENTS */
-
-const Course = ({course}) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-      {props.parts.map((part) => <Part key={part.id} part={part} />)}
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>{props.part.name} {props.part.exercises}</p>
-    </>
-  )
-}
-
-const Total = ({parts}) => {
-  const total = parts.reduce((total, part) => total + part.exercises, 0) // Initial value 0 for total
-
-  return (
-    <>
-      <p><b>Total number of exercises {total}</b></p>
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
