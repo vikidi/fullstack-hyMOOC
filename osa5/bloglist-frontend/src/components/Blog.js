8c749128b9954bef9e52d20f264cc8dac374d43f
Ville Saarinen <ville.saarinen9@gmail.com>
2020-09-22 15:51:22 +0300

    osa5 5.7, full view of blogs

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Blog = ({ blog }) => {
+  const [ fullView, setFullView ] = useState(false)
+
+  const toggleView = () => {
+    setFullView(!fullView)
+  }
+
+  const handleLike = () => {
+
+  }
+
+  const fullBlog = () => (
+    <div className='blog'>
+      {blog.title} {blog.author} <Button onClick={toggleView} text={fullView ? 'hide' : 'view'} /> <br></br>
+      {blog.url} <br></br>
+      likes {blog.likes} <Button onClick={handleLike} text='like' /> <br></br>
+      {blog.user.name}
+    </div>
+  )
+
+  const shortBlog = () => (
+    <div className='blog'>
+      {blog.title} {blog.author} <Button onClick={toggleView} text={fullView ? 'hide' : 'view'} />
+    </div>
+  )
+
+  return (
+    <div>
+      {fullView ? fullBlog() : shortBlog()}
+    </div>
+  )
+}
 
 export default Blog
