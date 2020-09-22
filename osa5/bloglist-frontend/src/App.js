6fb0cedfd52e7e9110751dada61adc54dbf3fe25
Ville Saarinen <ville.saarinen9@gmail.com>
2020-09-22 13:03:09 +0300

    osa5 5.3, refactor and create form

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,36 +1,18 @@
 import React, { useState, useEffect } from 'react'
 
 import Blog from './components/Blog'
+import CreateBlogForm from './components/CreateBlogForm'
+import LoginForm from './components/LoginForm'
 
 import blogService from './services/blogs'
-import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
-
-  const handleLogin = async (event) => {
-    event.preventDefault()
-
-    try {
-      const user = await loginService.login({ username, password })
-
-      window.localStorage.setItem('loggedUser', JSON.stringify(user)) 
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      //setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        //setErrorMessage(null)
-      }, 5000)
-    }
-  }
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedUser')
+    blogService.setToken(null)
     setUser(null)
   }
 
@@ -38,6 +20,7 @@ const App = () => {
     const loggedUserJSON = window.localStorage.getItem('loggedUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
+      blogService.setToken(user.token)
       setUser(user)
     }
   }, [])
@@ -49,43 +32,25 @@ const App = () => {
   }, [])
 
   if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-              <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-              <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    )
+    return ( <LoginForm setUser={setUser} /> )
   }
 
   return (
     <div>
       <h2>blogs</h2>
+
       {user.username} is logged in <button onClick={handleLogout}>logout</button>
+
       <br></br>
+
+      <CreateBlogForm />
+
       <br></br>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+
     </div>
   )
 }
---
+++ osa5/bloglist-frontend/src/components/CreateBlogForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+
+import blogService from '../services/blogs'
+
+const CreateBlogForm = ({ setBlogs }) => {
+    const [title, setTitle] = useState('') 
+    const [author, setAuthor] = useState('') 
+    const [url, setUrl] = useState('') 
+
+    const handleCreate = async (event) => {
+        event.preventDefault()
+
+        try {
+            await blogService.create({ title, author, url })
+            const blogs = await blogService.getAll()
+            setBlogs(blogs)
+
+            setTitle('')
+            setAuthor('')
+            setUrl('')
+        } catch (exception) {
+            //setErrorMessage('wrong credentials')
+            setTimeout(() => {
+            //setErrorMessage(null)
+            }, 5000)
+        }
+    }
+
+    return (
+        <div>
+          <h2>Create new blog</h2>
+          <form onSubmit={handleCreate}>
+            <div>
+              Title
+                <input
+                type="text"
+                value={title}
+                name="Title"
+                onChange={({ target }) => setTitle(target.value)}
+              />
+            </div>
+            <div>
+              Author
+                <input
+                type="text"
+                value={author}
+                name="Author"
+                onChange={({ target }) => setAuthor(target.value)}
+              />
+            </div>
+            <div>
+              Url
+                <input
+                type="text"
+                value={url}
+                name="Url"
+                onChange={({ target }) => setUrl(target.value)}
+              />
+            </div>
+            <button type="submit">Create</button>
+          </form>
+        </div>
+    )
+}
+
+export default CreateBlogForm
