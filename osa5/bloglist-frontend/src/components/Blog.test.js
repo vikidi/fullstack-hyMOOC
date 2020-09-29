d0c7a0f36ffa127b4c36661b06bf7482ace203bc
Ville Saarinen <ville.saarinen9@gmail.com>
2020-09-29 14:52:18 +0300

    osa5 5.14, likes and url after view click

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -22,5 +22,28 @@ describe('<Blog />', () => {
     expect(content).toContain(blog.author)
     expect(content).not.toContain(blog.likes)
     expect(content).not.toContain(blog.url)
+  })
+
+  test('Likes and url also shown after view click', async () => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'Ville Saarinen',
+      likes: 3,
+      url: 'localhost',
+      user: 'some user'
+    }
+
+    const component = render(
+      <Blog blog={blog} setBlogs={() => {}} loggedUser={{}} />
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    const content = component.container.querySelector('.blog').textContent
+    expect(content).toContain(blog.title)
+    expect(content).toContain(blog.author)
+    expect(content).toContain(blog.likes)
+    expect(content).toContain(blog.url)
   })
 })
