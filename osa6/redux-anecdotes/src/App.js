b6ce15c46b7050f445755f16718604638711ddf5
Ville Saarinen <ville.saarinen9@gmail.com>
2020-10-05 18:42:43 +0300

    osa6 6.8, anecdote list to own module

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,39 +1,14 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
 
 import AnecdoteForm from './components/AnecdoteForm'
+import AnacdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
-
-  const sortAnecdotes = () => {
-    anecdotes.sort((a, b) => b.votes - a.votes)
-    return anecdotes
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortAnecdotes().map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      
+      <AnacdoteList />
       <AnecdoteForm />
-
     </div>
   )
 }
