dcdbc75cc603846ca40cee86c6515e49cceeeb54
Ville Saarinen <ville.saarinen9@gmail.com>
2020-10-06 09:22:47 +0300

    osa6 6.10, init notifications

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnacdoteList from './components/AnecdoteList'
 
 const App = () => {
   return (
     <div>
+      <Notification />
       <h2>Anecdotes</h2>
       <AnacdoteList />
       <AnecdoteForm />
---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+const initialState = 'Test Notification'
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}
+
+export default reducer
