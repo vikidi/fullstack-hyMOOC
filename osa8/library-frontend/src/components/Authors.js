23366bbb49d0f0ffe368af2b69fc342e0c0929dc
Ville Saarinen <ville.saarinen9@gmail.com>
2020-11-05 13:13:17 +0200

    Forgot to remove .git, push remaining files

---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,43 @@
+  
+import React from 'react'
+import { useQuery } from '@apollo/client'
+
+import { ALL_AUTHORS } from '../services/queries'
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {!result.loading && 
+          result.data.allAuthors.map(a =>
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
